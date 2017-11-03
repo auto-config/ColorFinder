82904682ae66dd25b531f3a91807d1f137016db4
auto-config <32314328+auto-config@users.noreply.github.com>
2017-11-03 13:38:50 -0500

    Add files via upload

---
+++ ColorFinder6.lua
@@ -0,0 +1,9 @@
+--Color finder tool.
+local loc = Location(732, 364)
+local r, g, b = getColor(loc)
+starRegion = Region(0, 0, 715, 364)
+starRegion:highlight(3)
+wait(3)
+toast("Color: "..r..", "..g..", "..b..".")
+wait(5)
+scriptExit ("I am white text. This box has trapped me. HALP!")
