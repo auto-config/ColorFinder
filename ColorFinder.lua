48e322686dd326cf421dd1a16ca9f636db42ffb6
auto-config <32314328+auto-config@users.noreply.github.com>
2017-11-03 13:39:05 -0500

    Delete ColorFinder.lua

--- ColorFinder.lua
+++
@@ -1,9 +0,0 @@
---Color finder tool.
-local loc = Location(715, 364)
-local r, g, b = getColor(loc)
-starRegion = Region(0, 0, 715, 364)
-starRegion:highlight(3)
-wait(3)
-toast("Color: "..r..", "..g..", "..b..".")
-wait(5)
-scriptExit ("I am white text. This box has trapped me. HALP!")
